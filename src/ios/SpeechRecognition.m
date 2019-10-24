f3c4037c09922e5eb3d5f62b0ab0b074ae105f68
Murat Demirci <mdemirci01@gmail.com>
2019-10-25 01:02:30 +0300

    audiosession bug fix on ios

--- src/ios/SpeechRecognition.m
+++ src/ios/SpeechRecognition.m
@@ -84,7 +84,7 @@
         AVAudioSession *audioSession = [AVAudioSession sharedInstance];
         [audioSession setCategory:AVAudioSessionCategoryRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:nil];
         [audioSession setMode:AVAudioSessionModeMeasurement error:nil];
-        [audioSession setActive:NO withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
+        [audioSession setActive:YES withOptions:AVAudioSessionSetActiveOptionNotifyOthersOnDeactivation error:nil];
 
         self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
         self.recognitionRequest.shouldReportPartialResults = showPartial;
